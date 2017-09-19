16899d72c315dd04260ceca78f48cb7bd850b804
Omar Rwemi <omsharp@gmail.com>
2017-09-19 10:58:00 +0200

    Remove StringExtensions.cs from ignore list

---
+++ LinePress/Options/Infrastructure/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace LinePress.Options
+{
+   public static class StringExtensions
+   {
+      public static bool IsNullOrWhiteSpace(this string source)
+         => string.IsNullOrWhiteSpace(source);
+   }
+}
