5d0667832cf129fd960e1e60822384fb4a0f110b
Omar Rwemi <omsharp@gmail.com>
2017-09-21 20:48:04 +0200

    Clean up

--- LinePress/LineTransformation/LinePressTransformSource.cs
+++ LinePress/LineTransformation/LinePressTransformSource.cs
@@ -1,89 +1,87 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using LinePress.Options;
-using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Formatting;
 
 namespace LinePress
 {
-   public class LinePressTransformSource : ILineTransformSource
-   {
-      private readonly LineTransform defaultTransform = new LineTransform(0d, 0d, 1d);
+    public class LinePressTransformSource : ILineTransformSource
+    {
+        private readonly LineTransform defaultTransform = new LineTransform(0d, 0d, 1d);
 
-      private LineTransform emptyLineTransform;
-      private LineTransform customTokensTransform;
-      private LineTransform lineSpacingTransform;
+        private LineTransform emptyLineTransform;
+        private LineTransform customTokensTransform;
+        private LineTransform lineSpacingTransform;
 
-      private readonly IWpfTextView textView;
-      private readonly LinePressSettings settings = new LinePressSettings();
+        private readonly IWpfTextView textView;
+        private readonly LinePressSettings settings = new LinePressSettings();
 
-      private LinePressTransformSource(IWpfTextView view)
-      {
-         textView = view;
-         SettingsStore.LoadSettings(settings);
-         SettingsStore.SettingsChanged += OnSettingsChanged;
-         SetTransforms();
-      }
+        private LinePressTransformSource(IWpfTextView view)
+        {
+            textView = view;
+            SettingsStore.LoadSettings(settings);
+            SettingsStore.SettingsChanged += OnSettingsChanged;
+            SetTransforms();
+        }
 
-      private void OnSettingsChanged()
-      {
-         SettingsStore.LoadSettings(settings);
+        public static LinePressTransformSource Create(IWpfTextView view)
+        {
+            return view.Properties.GetOrCreateSingletonProperty(() => new LinePressTransformSource(view));
+        }
 
-         SetTransforms();
+        public LineTransform GetLineTransform(ITextViewLine line, double yPosition, ViewRelativePosition placement)
+        {
+            var lineText = line.Snapshot.GetText(line.Start, line.Length).Trim();
+            
+            if (IsComment(lineText))
+                return settings.ApplySpacingToComments ? lineSpacingTransform : defaultTransform;
 
-         var firstLine = textView.TextViewLines.FirstVisibleLine;
+            if (IsLongOrWrapped(line))
+                return lineSpacingTransform;
 
-         textView.DisplayTextLineContainingBufferPosition(firstLine.Start,
-                                                          firstLine.Top - textView.ViewportTop,
-                                                          ViewRelativePosition.Top);
-      }
+            if (settings.CompressEmptyLines && string.IsNullOrWhiteSpace(lineText))
+                return emptyLineTransform;
 
-      private void SetTransforms()
-      {
-         var lineHeight = textView?.TextViewLines?.FirstVisibleLine?.TextHeight ?? 10d;
-         var lineSpacing = (double)settings.LineSpacingPercent;
-         var pixelSpace = settings.LineSpacingPercent != 0 ? Math.Round(lineHeight * lineSpacing / 200d) : 0d;
-         
-         lineSpacingTransform = new LineTransform(pixelSpace, pixelSpace, 1d);
-         emptyLineTransform = new LineTransform(0d, 0d, EmptyLineScale);
-         customTokensTransform = new LineTransform(0d, 0d, CustomTokensScale);
-      }
+            if (settings.CompressCustomTokens && settings.CustomTokens.Contains(lineText))
+                return customTokensTransform;
 
-      public static LinePressTransformSource Create(IWpfTextView view)
-      {
-         return view.Properties.GetOrCreateSingletonProperty(() => new LinePressTransformSource(view));
-      }
-
-      public LineTransform GetLineTransform(ITextViewLine line, double yPosition, ViewRelativePosition placement)
-      {
-         var lineText = line.Snapshot.GetText(line.Start, line.Length).Trim();
-         
-         if (IsComment(lineText)) 
-            return settings.ApplySpacingToComments ? lineSpacingTransform : defaultTransform;
-         
-         if (IsLongOrWrapped(line)) 
             return lineSpacingTransform;
-         
-         if (settings.CompressEmptyLines && string.IsNullOrWhiteSpace(lineText))
-            return emptyLineTransform;
-         
-         if (settings.CompressCustomTokens && settings.CustomTokens.Contains(lineText))
-            return customTokensTransform;
-         
-         return lineSpacingTransform;
-      }
+        }
 
-      private bool IsComment(string codeLine) => Regex.Match(codeLine, @"^\/\/.*").Success;
-        
-      private bool IsLongOrWrapped(ITextViewLine line) => line.Length > 100 
-                                                       || !line.IsFirstTextViewLineForSnapshotLine 
-                                                       || !line.IsLastTextViewLineForSnapshotLine
-                                                       || line.End > line.Start.GetContainingLine().End;
+        private bool IsComment(string codeLine) => Regex.Match(codeLine, @"^\/\/.*").Success;
 
-      private double CustomTokensScale => (100d - settings.CustomTokensScale) / 100d;
-        
-      private double EmptyLineScale => (100d - settings.EmptyLineScale) / 100d;
+        private bool IsLongOrWrapped(ITextViewLine line) => line.Length > 100
+                                                         || !line.IsFirstTextViewLineForSnapshotLine
+                                                         || !line.IsLastTextViewLineForSnapshotLine
+                                                         || line.End > line.Start.GetContainingLine().End;
 
-   }
+        private double CustomTokensScale => (100d - settings.CustomTokensScale) / 100d;
+
+        private double EmptyLineScale => (100d - settings.EmptyLineScale) / 100d;
+
+        private void SetTransforms()
+        {
+            var lineHeight = textView?.TextViewLines?.FirstVisibleLine?.TextHeight ?? 10d;
+            var lineSpacing = (double)settings.LineSpacingPercent;
+            var pixelSpace = settings.LineSpacingPercent != 0 ? Math.Round(lineHeight * lineSpacing / 200d) : 0d;
+
+            lineSpacingTransform = new LineTransform(pixelSpace, pixelSpace, 1d);
+            emptyLineTransform = new LineTransform(0d, 0d, EmptyLineScale);
+            customTokensTransform = new LineTransform(0d, 0d, CustomTokensScale);
+        }
+
+        private void OnSettingsChanged()
+        {
+            SettingsStore.LoadSettings(settings);
+
+            SetTransforms();
+
+            var firstLine = textView.TextViewLines.FirstVisibleLine;
+
+            textView.DisplayTextLineContainingBufferPosition(firstLine.Start,
+                                                             firstLine.Top - textView.ViewportTop,
+                                                             ViewRelativePosition.Top);
+        }
+    }
 }
--- LinePress/Options/Infrastructure/SettingsStore.cs
+++ LinePress/Options/Infrastructure/SettingsStore.cs
@@ -9,114 +9,108 @@ using System.Reflection;
 
 namespace LinePress.Options
 {
-   public static class SettingsStore
-   {
-      private static readonly WritableSettingsStore store
-          = new ShellSettingsManager(ServiceProvider.GlobalProvider).GetWritableSettingsStore(SettingsScope.UserSettings);
+    public static class SettingsStore
+    {
+        private static readonly WritableSettingsStore store
+            = new ShellSettingsManager(ServiceProvider.GlobalProvider).GetWritableSettingsStore(SettingsScope.UserSettings);
 
-      public static event Action SettingsChanged;
+        public static event Action SettingsChanged;
 
-      public static void SaveSettings(ISettings settings)
-      {
-         try
-         {
-            if (!store.CollectionExists(settings.Key))
+        public static void SaveSettings(ISettings settings)
+        {
+            try
             {
-               store.CreateCollection(settings.Key);
+                if (!store.CollectionExists(settings.Key))
+                    store.CreateCollection(settings.Key);
+
+                if (SaveSettingsIntoStore(settings))
+                    SettingsChanged?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail(ex.Message);
+            }
+        }
+
+        public static void LoadSettings(ISettings settings)
+        {
+            try
+            {
+                if (store.CollectionExists(settings.Key))
+                    LoadSettingsFromStore(settings);
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail(ex.Message);
+            }
+        }
+
+        private static bool SaveSettingsIntoStore(ISettings settings)
+        {
+            var saved = false;
+
+            foreach (var prop in GetProperties(settings))
+            {
+                switch (prop.GetValue(settings))
+                {
+                    case bool b:
+                        store.SetBoolean(settings.Key, prop.Name, b);
+                        saved = true;
+                        break;
+
+                    case int i:
+                        store.SetInt32(settings.Key, prop.Name, i);
+                        saved = true;
+                        break;
+
+                    case double d:
+                        store.SetString(settings.Key, prop.Name, d.ToString());
+                        saved = true;
+                        break;
+
+                    case string s:
+                        store.SetString(settings.Key, prop.Name, s);
+                        saved = true;
+                        break;
+                }
             }
 
-            if (SaveSettingsIntoStore(settings))
+            return saved;
+        }
+
+        private static void LoadSettingsFromStore(ISettings settings)
+        {
+            foreach (var prop in GetProperties(settings))
             {
-               SettingsChanged?.Invoke();
+                if (store.PropertyExists(settings.Key, prop.Name))
+                {
+                    switch (prop.GetValue(settings))
+                    {
+                        case bool b:
+                            prop.SetValue(settings, store.GetBoolean(settings.Key, prop.Name));
+                            break;
+
+                        case int i:
+                            prop.SetValue(settings, store.GetInt32(settings.Key, prop.Name));
+                            break;
+
+                        case double d when double.TryParse(store.GetString(settings.Key, prop.Name), out double value):
+                            prop.SetValue(settings, value);
+                            break;
+
+                        case string s:
+                            prop.SetValue(settings, store.GetString(settings.Key, prop.Name));
+                            break;
+                    }
+                }
             }
-         }
-         catch (Exception ex)
-         {
-            Debug.Fail(ex.Message);
-         }
-      }
+        }
 
-      public static void LoadSettings(ISettings settings)
-      {
-         try
-         {
-            if (store.CollectionExists(settings.Key))
-            {
-               LoadSettingsFromStore(settings);
-            }
-         }
-         catch (Exception ex)
-         {
-            Debug.Fail(ex.Message);
-         }
-      }
-
-      private static bool SaveSettingsIntoStore(ISettings settings)
-      {
-         var saved = false;
-
-         foreach (var prop in GetProperties(settings))
-         {
-            switch (prop.GetValue(settings))
-            {
-               case bool b:
-                  store.SetBoolean(settings.Key, prop.Name, b);
-                  saved = true;
-                  break;
-
-               case int i:
-                  store.SetInt32(settings.Key, prop.Name, i);
-                  saved = true;
-                  break;
-
-               case double d:
-                  store.SetString(settings.Key, prop.Name, d.ToString());
-                  saved = true;
-                  break;
-
-               case string s:
-                  store.SetString(settings.Key, prop.Name, s);
-                  saved = true;
-                  break;
-            }
-         }
-
-         return saved;
-      }
-
-      private static void LoadSettingsFromStore(ISettings settings)
-      {
-         foreach (var prop in GetProperties(settings))
-         {
-            if (store.PropertyExists(settings.Key, prop.Name))
-            {
-               switch (prop.GetValue(settings))
-               {
-                  case bool b:
-                     prop.SetValue(settings, store.GetBoolean(settings.Key, prop.Name));
-                     break;
-
-                  case int i:
-                     prop.SetValue(settings, store.GetInt32(settings.Key, prop.Name));
-                     break;
-
-                  case double d when double.TryParse(store.GetString(settings.Key, prop.Name), out double value):
-                     prop.SetValue(settings, value);
-                     break;
-
-                  case string s:
-                     prop.SetValue(settings, store.GetString(settings.Key, prop.Name));
-                     break;
-               }
-            }
-         }
-      }
-
-      private static IEnumerable<PropertyInfo> GetProperties(ISettings settings)
-      {
-         return settings.GetType()
-                        .GetProperties()
-                        .Where(p => Attribute.IsDefined(p, typeof(SettingAttribute)));
-      }
-   }
+        private static IEnumerable<PropertyInfo> GetProperties(ISettings settings)
+        {
+            return settings.GetType()
+                           .GetProperties()
+                           .Where(p => Attribute.IsDefined(p, typeof(SettingAttribute)));
+        }
+    }
 }
