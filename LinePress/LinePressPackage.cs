422638e137586add50775d4444e2f16d250966eb
Omar Rwemi <omsharp@gmail.com>
2019-04-04 19:48:23 +0200

    Derive LinePressPackage from AsyncPackage

--- LinePress/LinePressPackage.cs
+++ LinePress/LinePressPackage.cs
@@ -13,7 +13,7 @@ namespace LinePress
    [Guid(PackageGuidString)]
    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly",
                     Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-   public sealed class LinePressPackage : Package
+   public sealed class LinePressPackage : AsyncPackage
    {
       public const string PackageGuidString = "0267e91e-033e-4dbc-9f41-0efe67f515ae";
    }
